7640496d00111db0bc3b0a7e7b1433cc269c40fc
Rexavery <archilaps@gmail.com>
2025-07-19 21:36:09 +0100

    Create machines.module.ts

---
+++ src/machines/machines.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MachinesController } from './machines.controller';
+import { MachinesService } from './machines.service';
+import { Machine } from '../entities/machine.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Machine])],
+  controllers: [MachinesController],
+  providers: [MachinesService],
+})
+export class MachinesModule {}
