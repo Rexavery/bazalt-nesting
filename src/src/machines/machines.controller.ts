ce44298c4d0f26b178f7c81264e0191446cd679e
Rexavery <archilaps@gmail.com>
2025-07-19 21:37:00 +0100

    Delete src/src/machines directory

--- src/src/machines/machines.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { MachinesService } from './machines.service';
-
-@Controller('machines')
-export class MachinesController {
-  constructor(private readonly service: MachinesService) {}
-  @Get() async findAll() { return this.service.findAll(); }
-  @Post() async create(@Body() dto: any) { return this.service.create(dto); }
-}
